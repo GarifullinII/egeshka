2ffe60b93899eaeeab2b6912beaa51ee72e0b2dd
GarifullinII <ildaridrisovich@gmail.com>
2025-06-02 16:52:19 +0400

    initial
    initial commit

---
+++ egeshka/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  egeshka
+//
+//  Created by Ildar Garifullin on 02.06.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+}
+
